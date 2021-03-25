6a10668668bff5740abc872fdfadeef736dd1adf
Philly <Phil.stew@live.ca>
2021-03-25 01:52:00 -0400

    added button and submit

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,5 +1,8 @@
-dateEl= document.getElementById('date')
-timeEl= document.getElementById('timeSet')
+dateEl= document.getElementById('date');
+timeEl= document.getElementById('timeSet');
+workTime= document.getElementById('worktime');
+submitBtn = document.getElementById('button-addon2')
+
 
 
 
@@ -7,17 +10,20 @@ timeEl= document.getElementById('timeSet')
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     dateEl.textContent= (rightNow);
-  }
 
-  function printDayTask(task, time ) {
+  }function printDayTask(task, time ) {
+  var hourToday = moment(time, 'LT').diff(moment(), 'days');
+  };
 
-      var hourToday = moment(time, 'LT').diff(moment(), 'days');
+ setInterval(displayTime, 1000);
+  
+function localStorage(){
+    event.preventDefault();
 
-  }
+}
 
 
 
 
-
-
-  setInterval(displayTime, 1000);
+var timeDay = moment().startOff(9)
+workTime.textContent= (timeDay);
