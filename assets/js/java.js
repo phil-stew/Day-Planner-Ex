2966738780095d75219539850a5ffb6c11518e62
Philly <Phil.stew@live.ca>
2021-03-25 21:21:06 -0400

    final got one box to save

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,29 +1,62 @@
-dateEl= document.getElementById('date');
+dateEl= document.getElementById('TodaysDate');
 timeEl= document.getElementById('timeSet');
 workTime= document.getElementById('worktime');
 submitBtn = document.getElementById('button-addon2')
+inputDayPlanEl = document.getElementById('text');
 
 
 
 
+savingItems();
+
+
+submitBtn.addEventListener('click', function(event){
+    event.preventDefault();
+
+    var dayPlannner = document.getElementById('text').value;
+    localStorage.setItem("dayplan" , dayPlannner);
+    var dayPlans = localStorage.getItem("dayplan").value;
+    dayPlanEl.textContent = dayPlans;
+    console.log(dayPlans)
+
+   });
+
+function savingItems(){
+    dayPlanEl = document.getElementById('text');
+    activityData= dayPlanEl.value 
+
+    
+     var activityData = localStorage.getItem("dayplan");
+     document.getElementById('text').value = activityData;
+
+     console.log(activityData)
+     
+     console.log(dayPlanEl)
+     console.log("dayplan")
+}
 
 function displayTime() {
+    
+    
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     dateEl.textContent= (rightNow);
-
-  }function printDayTask(task, time ) {
-  var hourToday = moment(time, 'LT').diff(moment(), 'days');
-  };
-
- setInterval(displayTime, 1000);
-  
-function localStorage(){
-    event.preventDefault();
 
 }
 
 
 
+ function displayHour(){
+ var timeDay = moment.locale();
+ workTime.textContent= (timeDay);
 
-var timeDay = moment().startOff(9)
-workTime.textContent= (timeDay);
+}
+  
+
+
+ 
+
+
+ setInterval(displayTime, 1000);
+
+ displayHour();
+ savingItems();
