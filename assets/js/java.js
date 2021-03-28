49ca87893788d9b623fcdec3288627f189ef4cd3
Philly <Phil.stew@live.ca>
2021-03-28 18:24:43 -0400

    updating code

--- assets/js/java.js
+++ assets/js/java.js
@@ -10,23 +10,34 @@ inputDayPlanEl = document.getElementById('text');
 savingItems();
 
 
-submitBtn.addEventListener('click', function(event){
-    event.preventDefault();
+//submitBtn.addEventListener('click', function(event){
+    //event.preventDefault();
+
+    //var timePlaned = 9
+        
+    
+$(".btn").click(function (e) { 
+    e.preventDefault();
+    console.log(this)
+    
+    this.value = dayPlan
+    console.log(dayPlan)
+
+
 
     var dayPlannner = document.getElementById('text').value;
-    localStorage.setItem("dayplan" , dayPlannner);
-    var dayPlans = localStorage.getItem("dayplan").value;
+    localStorage.setItem(dayPlan , dayPlannner);
+    var dayPlans = localStorage.getItem(dayPlan).value;
     dayPlanEl.textContent = dayPlans;
     console.log(dayPlans)
 
-   });
+});
 
 function savingItems(){
+    
     dayPlanEl = document.getElementById('text');
     activityData= dayPlanEl.value 
-
-    
-     var activityData = localStorage.getItem("dayplan");
+     var activityData = localStorage.getItem(dayPlan);
      document.getElementById('text').value = activityData;
 
      console.log(activityData)
@@ -40,6 +51,7 @@ function displayTime() {
     
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     dateEl.textContent= (rightNow);
+
 
 }
 
