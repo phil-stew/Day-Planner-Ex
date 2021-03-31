86389e06b00ea2368f9a27dfe3c4c9ee2daac19d
Philly <Phil.stew@live.ca>
2021-03-31 12:56:22 -0400

    finalizing js

--- assets/js/java.js
+++ assets/js/java.js
@@ -12,8 +12,6 @@ $(".saveBtn").on("click",function () {
     var time = $(this).parent().attr('id')
 
     localStorage.setItem(time,input)
-
-    
  
 });
 
@@ -44,16 +42,15 @@ var check = setInterval(displayEvent,10000)
 
 
 $('#hour-9 .description').val(localStorage.getItem('hour-9'))
-$('#hour-9 .description').val(localStorage.getItem('hour-10'))
-$('#hour-9 .description').val(localStorage.getItem('hour-11'))
-$('#hour-9 .description').val(localStorage.getItem('hour-12'))
-$('#hour-9 .description').val(localStorage.getItem('hour-13'))
-$('#hour-9 .description').val(localStorage.getItem('hour-14'))
-$('#hour-9 .description').val(localStorage.getItem('hour-15'))
-$('#hour-9 .description').val(localStorage.getItem('hour-16'))
-$('#hour-9 .description').val(localStorage.getItem('hour-17'))
-$('#hour-9 .description').val(localStorage.getItem('hour-18'))
-
+$('#hour-10 .description').val(localStorage.getItem('hour-10'))
+$('#hour-11 .description').val(localStorage.getItem('hour-11'))
+$('#hour-12 .description').val(localStorage.getItem('hour-12'))
+$('#hour-13 .description').val(localStorage.getItem('hour-13'))
+$('#hour-14 .description').val(localStorage.getItem('hour-14'))
+$('#hour-15 .description').val(localStorage.getItem('hour-15'))
+$('#hour-16 .description').val(localStorage.getItem('hour-16'))
+$('#hour-17 .description').val(localStorage.getItem('hour-17'))
+$('#hour-18 .description').val(localStorage.getItem('hour-18'))
 
 
 function displayTime() {
